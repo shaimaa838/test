82a55bcb5cc4a751fd8ea1abf72751dab5f63ece
shaimaa.salah <shaymaasalah196@gmail.com>
2020-02-13 22:28:57 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit, HostListener } from '@angular/core';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { ResizedEvent } from 'angular-resize-event';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent       {
+
+
   title = 'drag';
+  connectedGroups = new Array();
+  projectFields = [
+    {name:"Item Name", width:100},
+    {name:"Status", width:200},
+    {name:"Assignee", width:300},
+    {name:"Estimate", width:100},
+    {name:"Progress", width:200}
+  ];
+  groups = [{ name:'To Do', items : [
+    'Task 1',
+    'Task 2',
+    'Task 3'
+  ]},
+  {name:"In Progress", items:['Task 4',
+  'Task 5',
+  'Task 6']},
+  {name:"Done", items:['Task 7',
+  'Task 8',
+  'Task 9']}
+]; 
+ 
+changeText = false;
+resizing = true;
+
+  dropItem(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+  headerDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.projectFields, event.previousIndex, event.currentIndex);
+  }
+  width: number;
+  height: number;
+ 
+clicked(i)
+{
+  console.log(i);
+
 }
+  onResized(event: ResizedEvent,index) {
+   
+/*     console.log(this.changeText);
+console.log(event.newWidth-event.oldWidth)
+let oldWidth=event.oldWidth==undefined?0:event.oldWidth;
+   if(this.changeText && event.newWidth-oldWidth<100)
+     {
+        this.projectFields[i].width = event.newWidth; 
+       //this.changeText=false;
+    }
+    */
+
+   for(var i=0; i<=this.groups.length; i++)  {
+    
+     for(var j=0; j<this.projectFields.length;j++){
+     // document.querySelector("#col-"+i+"-"+j).removeEventListener("resized", this.onResized, true);
+   
+     }
+   } 
+   
+    
+   this.projectFields[index].width= event.newWidth;
+    
+  }
+}
+  
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularResizedEventModule } from 'angular-resize-event';
+import { SplitterModule } from '@syncfusion/ej2-angular-layouts';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule ,
+    ScrollingModule,
+    CdkTableModule,
+    CdkTreeModule,
+    FormsModule,
+    AngularResizedEventModule,
+    SplitterModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
